658ef0a9c3cc0c12d338237d690b1ac591f5c693
xMedox <tim@g-lotz.de>
2016-03-13 14:13:07 +0100

    Update Skybox class

--- src/net/medox/neonengine/rendering/Skybox.java
+++ src/net/medox/neonengine/rendering/Skybox.java
@@ -8,59 +8,46 @@ public class Skybox extends EntityComponent{
 	private Mesh mesh;
 	private Material material;
 	
-	private float size;
-	
 	public Skybox(String right, String left, String top, String bottom, String front, String back){
 		this(right, left, top, bottom, front, back, false);
 	}
 	
 	public Skybox(String right, String left, String top, String bottom, String front, String back, boolean nearest){
-		this.size = 50.0f;
+		float size = 50.0f;
 		
 		material = new Material();
 		
-		String[] s = new String[6];
+		material.setCubeMap("cubeMap", new CubeMap(new String[]{right, left, top, bottom, front, back}, RenderingEngine.TEXTURE_2D, nearest ? RenderingEngine.NEAREST : RenderingEngine.LINEAR, RenderingEngine.RGBA, RenderingEngine.RGBA, true));
 		
-		s[0] = right;
-		s[1] = left;
-		s[2] = top;
-		s[3] = bottom;
-		s[4] = front;
-		s[5] = back;
+		final float vertexMin = -size/2-1f;
+		final float vertexMax = size/2+1f;
 		
-		final CubeMap cube = new CubeMap(s, RenderingEngine.TEXTURE_2D, nearest ? RenderingEngine.NEAREST : RenderingEngine.LINEAR, RenderingEngine.RGBA, RenderingEngine.RGBA, true);
+		final Vector3f[] vertices = new Vector3f[]{new Vector3f(vertexMin, vertexMax, vertexMin),
+												   new Vector3f(vertexMin, vertexMax, vertexMax),
+												   new Vector3f(vertexMin, vertexMin, vertexMax),
+												   new Vector3f(vertexMin, vertexMin, vertexMin),
+												   new Vector3f(vertexMax, vertexMax, vertexMin),
+												   new Vector3f(vertexMin, vertexMax, vertexMin),
+												   new Vector3f(vertexMin, vertexMin, vertexMin),
+												   new Vector3f(vertexMax, vertexMin, vertexMin),
+												   new Vector3f(vertexMax, vertexMax, vertexMax),
+												   new Vector3f(vertexMax, vertexMax, vertexMin),
+												   new Vector3f(vertexMax, vertexMin, vertexMin),
+												   new Vector3f(vertexMax, vertexMin, vertexMax),
+												   new Vector3f(vertexMin, vertexMax, vertexMax),
+												   new Vector3f(vertexMax, vertexMax, vertexMax),
+												   new Vector3f(vertexMax, vertexMin, vertexMax),
+												   new Vector3f(vertexMin, vertexMin, vertexMax),
+												   new Vector3f(vertexMin, vertexMax, vertexMin),
+												   new Vector3f(vertexMax, vertexMax, vertexMin),
+												   new Vector3f(vertexMax, vertexMax, vertexMax),
+												   new Vector3f(vertexMin, vertexMax, vertexMax),
+												   new Vector3f(vertexMax, vertexMin, vertexMin),
+												   new Vector3f(vertexMin, vertexMin, vertexMin),
+												   new Vector3f(vertexMin, vertexMin, vertexMax),
+												   new Vector3f(vertexMax, vertexMin, vertexMax)};
 		
-		material.setCubeMap("cubeMap", cube);
-		
-		final float vm = -size/2-1f;
-		final float vp = size/2+1f;
-		
-		final Vector3f[] v = new Vector3f[]{new Vector3f(vm, vp, vm),
-								   new Vector3f(vm, vp, vp),
-								   new Vector3f(vm, vm, vp),
-								   new Vector3f(vm, vm, vm),
-								   new Vector3f(vp, vp, vm),
-								   new Vector3f(vm, vp, vm),
-								   new Vector3f(vm, vm, vm),
-								   new Vector3f(vp, vm, vm),
-								   new Vector3f(vp, vp, vp),
-					  			   new Vector3f(vp, vp, vm),
-					  			   new Vector3f(vp, vm, vm),
-					  			   new Vector3f(vp, vm, vp),
-					  			   new Vector3f(vm, vp, vp),
-								   new Vector3f(vp, vp, vp),
-								   new Vector3f(vp, vm, vp),
-								   new Vector3f(vm, vm, vp),
-								   new Vector3f(vm, vp, vm),
-								   new Vector3f(vp, vp, vm),
-								   new Vector3f(vp, vp, vp),
-								   new Vector3f(vm, vp, vp),
-								   new Vector3f(vp, vm, vm),
-								   new Vector3f(vm, vm, vm),
-								   new Vector3f(vm, vm, vp),
-								   new Vector3f(vp, vm, vp)};
-		
-		mesh = getMesh(v);
+		mesh = getMesh(vertices);
 		
 		RenderingEngine.setMainSkybox(this);
 	}
@@ -75,20 +62,20 @@ public class Skybox extends EntityComponent{
 		mesh.addFace(0, 1, 2);
 		mesh.addFace(0, 2, 3);
 		
-		mesh.addFace(0+4, 1+4, 2+4);
-		mesh.addFace(0+4, 2+4, 3+4);
+		mesh.addFace(4, 5, 6);
+		mesh.addFace(4, 6, 7);
 		
-		mesh.addFace(0+8, 1+8, 2+8);
-		mesh.addFace(0+8, 2+8, 3+8);
+		mesh.addFace(8, 9, 10);
+		mesh.addFace(8, 10, 11);
 		
-		mesh.addFace(0+12, 1+12, 2+12);
-		mesh.addFace(0+12, 2+12, 3+12);
+		mesh.addFace(12, 13, 14);
+		mesh.addFace(12, 14, 15);
 		
-		mesh.addFace(0+16, 1+16, 2+16);
-		mesh.addFace(0+16, 2+16, 3+16);
+		mesh.addFace(16, 17, 18);
+		mesh.addFace(16, 18, 19);
 		
-		mesh.addFace(0+20, 1+20, 2+20);
-		mesh.addFace(0+20, 2+20, 3+20);
+		mesh.addFace(20, 21, 22);
+		mesh.addFace(20, 22, 23);
 				
 		return new Mesh("", mesh.finalizeModel());
 	}
