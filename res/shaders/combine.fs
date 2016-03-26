27c05be61844c311fe60e9a4cb10ecbd66d0063c
xMedox <tim@g-lotz.de>
2016-03-26 18:35:47 +0100

    Tested some bloom stuff

--- res/shaders/combine.fs
+++ res/shaders/combine.fs
@@ -3,10 +3,15 @@
 in vec2 texCoord0;
 
 uniform sampler2D R0_displayTexture;
+//uniform sampler2D R1_displayTexture;
 uniform sampler2D R_filterTexture;
 
 layout(location = 0) out vec4 outputFS;
 
 void main(){
-	outputFS = texture(R0_displayTexture, texCoord0) + texture(R_filterTexture, texCoord0);
+	//if(texture(R1_displayTexture, texCoord0) == vec4(0, 0, 0, 0)){
+		outputFS = texture(R0_displayTexture, texCoord0) + texture(R_filterTexture, texCoord0);
+	//}else{
+	//	outputFS = texture(R0_displayTexture, texCoord0);
+	//}
 }
