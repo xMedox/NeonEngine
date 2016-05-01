c3731f10b63fb79d60b9be3411ca57de537ae9f4
xMedox <tim@g-lotz.de>
2016-05-01 15:26:02 +0200

    Finished rewriting the TrueTypeFont class

--- src/net/medox/neonengine/rendering/TrueTypeFont.java
+++ src/net/medox/neonengine/rendering/TrueTypeFont.java
@@ -60,14 +60,14 @@ public class TrueTypeFont{
     public void setCorrection(boolean on){
         if(on){
         	correctL = 2;
-            correctR = 1;
+        	correctR = 1;
         }else{
         	correctL = 0;
-            correctR = 0;
+        	correctR = 0;
         }
     }
     
-    private BufferedImage getFontImage(char ch){
+	private BufferedImage getFontImage(char ch){
         BufferedImage tempfontImage = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
         Graphics2D g = (Graphics2D)tempfontImage.getGraphics();
         
@@ -108,11 +108,11 @@ public class TrueTypeFont{
         return fontImage;
     }
     
-    @SuppressWarnings("unchecked")
+	@SuppressWarnings("unchecked")
 	private void createSet(char[] customCharsArray){
-    	if(customCharsArray != null && customCharsArray.length > 0){
-    		textureWidth *= 2;
-    	}
+		if(customCharsArray != null && customCharsArray.length > 0){
+			textureWidth *= 2;
+		}
     	
     	try{
     		BufferedImage imgTemp = new BufferedImage(textureWidth, textureHeight, BufferedImage.TYPE_INT_ARGB);
@@ -174,7 +174,7 @@ public class TrueTypeFont{
     	}
 	}
     
-    private void drawQuad(float drawX, float drawY, float drawX2, float drawY2, float srcX, float srcY, float srcX2, float srcY2, Vector3f color){
+	private void drawQuad(float drawX, float drawY, float drawX2, float drawY2, float srcX, float srcY, float srcX2, float srcY2, Vector3f color){
     	float DrawWidth = drawX2 - drawX;
     	float DrawHeight = drawY2 - drawY;
     	float TextureSrcX = srcX / textureWidth;
@@ -186,7 +186,7 @@ public class TrueTypeFont{
         
     	Transform2D t2 = new Transform2D();
 		
-    	t2.setPos(new Vector2f((drawX+DrawWidth), (drawY)));
+    	t2.setPos(new Vector2f(drawX + DrawWidth, drawY));
     	t2.setScale(new Vector2f(-DrawWidth, DrawHeight));
 		
     	if(RenderingEngine.mesh2DInFrustum(t2)){
@@ -201,11 +201,12 @@ public class TrueTypeFont{
 		
 		for(int i = 0; i < whatchars.length(); i++){
 			currentChar = whatchars.charAt(i);
+			
 			if(currentChar < 256){
 				intObject = charArray[currentChar];
-            }else{
-            	intObject = (IntObject)customChars.get(new Character((char) currentChar));
-            }
+			}else{
+				intObject = (IntObject)customChars.get(new Character((char)currentChar));
+			}
 			
 			if(intObject != null){
 				totalwidth += intObject.width;
@@ -227,171 +228,134 @@ public class TrueTypeFont{
 	}
 	
 	public void drawString(float x, float y, String whatchars, float scaleX, float scaleY){
-		drawString(x,y,whatchars, 0, whatchars.length()-1, scaleX, scaleY, new Vector3f(1, 1, 1), ALIGN_LEFT);
+		drawString(x, y, whatchars, 0, whatchars.length()-1, scaleX, scaleY, new Vector3f(1, 1, 1), ALIGN_LEFT);
 	}
 	
 	public void drawString(float x, float y, String whatchars, float scaleX, float scaleY, Vector3f color){
-		drawString(x,y,whatchars, 0, whatchars.length()-1, scaleX, scaleY, color, ALIGN_LEFT);
+		drawString(x, y, whatchars, 0, whatchars.length()-1, scaleX, scaleY, color, ALIGN_LEFT);
 	}
 	
 	public void drawString(float x, float y, String whatchars, float scaleX, float scaleY, Vector3f color, int format){
-		drawString(x,y,whatchars, 0, whatchars.length()-1, scaleX, scaleY, color, format);
+		drawString(x, y, whatchars, 0, whatchars.length()-1, scaleX, scaleY, color, format);
 	}
 	
-    public void drawString(float x, float y,
-            String whatchars, int startIndex, int endIndex,
-            float scaleX, float scaleY, Vector3f color,
-            int format
-            ){
-        IntObject intObject = null;
-        int charCurrent;
-         
- 
-        int totalwidth = 0;
-        int i = startIndex, d, c;
-        float startY = 0;
- 
- 
-         
-        switch (format) {
-            case ALIGN_RIGHT: {
-                d = -1;
-                c = correctR;
-             
-                while (i < endIndex) {
-                    if (whatchars.charAt(i) == '\n') startY -= fontHeight;
-                    i++;
-                }
-                break;
-            }
-            case ALIGN_CENTER: {
-                for (int l = startIndex; l <= endIndex; l++) {
-                    charCurrent = whatchars.charAt(l);
-                    if (charCurrent == '\n') break;
-                    if (charCurrent < 256) {
-                        intObject = charArray[charCurrent];
-                    } else {
-                        intObject = (IntObject)customChars.get( new Character( (char) charCurrent ) );
-                    }
-                    totalwidth += intObject.width-correctL;
-                }
-                totalwidth /= -2;
-            }
-            case ALIGN_LEFT:
-            default: {
-                d = 1;
-                c = correctL;
-                break;
-            }
-         
-        }
-         
-        while (i >= startIndex && i <= endIndex) {
-             
-            charCurrent = whatchars.charAt(i);
-            if (charCurrent < 256) {
-                intObject = charArray[charCurrent];
-            } else {
-                intObject = (IntObject)customChars.get( new Character( (char) charCurrent ) );
-            } 
-             
-            if( intObject != null ) {
-                if (d < 0) totalwidth += (intObject.width-c) * d;
-                    if (charCurrent == '\n') {
-                        startY -= fontHeight * d;
-                        totalwidth = 0;
-                        if (format == ALIGN_CENTER) {
-                            for (int l = i+1; l <= endIndex; l++) {
-                                charCurrent = whatchars.charAt(l);
-                                if (charCurrent == '\n') break;
-                                if (charCurrent < 256) {
-                                    intObject = charArray[charCurrent];
-                                } else {
-                                    intObject = (IntObject)customChars.get( new Character( (char) charCurrent ) );
-                                }
-                                totalwidth += intObject.width-correctL;
-                            }
-                            totalwidth /= -2;
-                        }
-                        //if center get next lines total width/2;
-                    }
-                    else {
-                        drawQuad((totalwidth + intObject.width) * scaleX + x, startY * scaleY + y,
-                            totalwidth * scaleX + x,
-                            (startY + intObject.height) * scaleY + y, intObject.storedX + intObject.width,
-                            intObject.storedY + intObject.height,intObject.storedX, 
-                            intObject.storedY, color);
-                        if (d > 0) totalwidth += (intObject.width-c) * d + 1; //TODO remove +1?
-                    }
-                    i += d;
-                 
-            }
-        }
-        
-//		RenderingEngine.setVector2f("xMax", new Vector2f(1f, 0f));
-//		RenderingEngine.setVector2f("yMax", new Vector2f(1f, 0f));
-        
-//        ArrayList<Vector2f> texCoords = new ArrayList<Vector2f>();
-//		
-//		texCoords.add(new Vector2f(1f, 0f));
-//		texCoords.add(new Vector2f(0f, 0f));
-//		texCoords.add(new Vector2f(1f, 1f));
-//		texCoords.add(new Vector2f(0f, 1f));
-//		
-//		mesh.lel(texCoords);
+	public void drawString(float x, float y, String whatchars, int startIndex, int endIndex, float scaleX, float scaleY, Vector3f color, int format){
+		IntObject intObject = null;
+		int charCurrent;
 		
-//       shader.bind();
-//		
-//		t.setPos(new Vector3f((int)250, (int)250, 0.5f));
-////	        t.setPos(new Vector3f((int)100, (int)100, 0.5f));
-//		t.setScale(new Vector3f((int)500, (int)500, 1));
-//		
-//		m.setTexture("diffuse", fontTexture);
-//		
-////			ArrayList<Vector2f> texCoords = new ArrayList<Vector2f>();
-////			
-////			texCoords.add(new Vector2f(TextureSrcX, TextureSrcY + RenderHeight));
-////			texCoords.add(new Vector2f(TextureSrcX + RenderWidth, TextureSrcY + RenderHeight));
-////			texCoords.add(new Vector2f(TextureSrcX, TextureSrcY));
-////			texCoords.add(new Vector2f(TextureSrcX + RenderWidth, TextureSrcY));
-//		
-////			Vector2f[] t12 = new Vector2f[]{new Vector2f(TextureSrcX, TextureSrcY + RenderHeight),
-////					   new Vector2f(TextureSrcX + RenderWidth, TextureSrcY + RenderHeight),
-////					   new Vector2f(TextureSrcX, TextureSrcY),
-////					   new Vector2f(TextureSrcX + RenderWidth, TextureSrcY)};
-//		
-////			mesh.lel(texCoords);
-//		
-//		shader.updateUniforms(t, m, camera);
-//		mesh.draw();
-    }
-    
-    public static boolean isSupported(String fontname) {
-        Font font[] = getFonts();
-        for (int i = font.length-1; i >= 0; i--) {
-            if (font[i].getName().equalsIgnoreCase(fontname))
-                return true;
-        }
-        return false;
-    }
-    
-    public static Font[] getFonts() {
-        return GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
-    }
-    
-    public static byte[] intToByteArray(int value) {
-        return new byte[] {
-                (byte)(value >>> 24),
-                (byte)(value >>> 16),
-                (byte)(value >>> 8),
-                (byte)value};
-    }
-    
+		int totalwidth = 0;
+		int i = startIndex, d, c;
+		float startY = 0;
+		
+		switch(format){
+			case ALIGN_RIGHT:{
+				d = -1;
+				c = correctR;
+				
+				while(i < endIndex){
+					if(whatchars.charAt(i) == '\n'){
+						startY -= fontHeight;
+					}
+					i++;
+				}
+				break;
+			}
+			case ALIGN_CENTER:{
+				for(int l = startIndex; l <= endIndex; l++){
+					charCurrent = whatchars.charAt(l);
+					
+					if(charCurrent == '\n'){
+						break;
+					}
+					
+					if(charCurrent < 256){
+						intObject = charArray[charCurrent];
+					}else{
+						intObject = (IntObject)customChars.get(new Character((char) charCurrent));
+					}
+					
+					totalwidth += intObject.width-correctL;
+				}
+				totalwidth /= -2;
+			}
+			case ALIGN_LEFT:
+				default:{
+					d = 1;
+					c = correctL;
+					break;
+	            }
+		}
+		
+		while(i >= startIndex && i <= endIndex){
+			charCurrent = whatchars.charAt(i);
+			if(charCurrent < 256){
+				intObject = charArray[charCurrent];
+			}else{
+				intObject = (IntObject)customChars.get(new Character((char)charCurrent));
+			} 
+			
+			if(intObject != null){
+				if(d < 0){
+					totalwidth += (intObject.width-c) * d;
+				}
+				
+				if(charCurrent == '\n'){
+					startY -= fontHeight * d;
+					totalwidth = 0;
+					if(format == ALIGN_CENTER){
+						for(int l = i+1; l <= endIndex; l++){
+							charCurrent = whatchars.charAt(l);
+							
+							if(charCurrent == '\n'){
+								break;
+							}
+							
+							if(charCurrent < 256){
+								intObject = charArray[charCurrent];
+							}else{
+								intObject = (IntObject)customChars.get(new Character((char)charCurrent));
+							}
+							
+							totalwidth += intObject.width-correctL;
+						}
+						totalwidth /= -2;
+					}
+					
+				}else{
+					drawQuad((totalwidth + intObject.width) * scaleX + x, startY * scaleY + y, totalwidth * scaleX + x, (startY + intObject.height) * scaleY + y, intObject.storedX + intObject.width, intObject.storedY + intObject.height,intObject.storedX, intObject.storedY, color);
+					
+					if(d > 0){
+						totalwidth += (intObject.width-c) * d + 1; //TODO remove +1?
+					}
+				}
+				i += d;
+			}
+		}
+	}
+	
+	public static boolean isSupported(String fontname){
+		Font font[] = getFonts();
+		for(int i = font.length-1; i >= 0; i--){
+			if(font[i].getName().equalsIgnoreCase(fontname)){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static Font[] getFonts(){
+		return GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+	}
+	
+	public static byte[] intToByteArray(int value){
+		return new byte[]{(byte)(value >>> 24), (byte)(value >>> 16), (byte)(value >>> 8), (byte)value};
+	}
+	
 	private class IntObject{
 		public int width;
 		public int height;
 		
 		public int storedX;
 		public int storedY;
-    }
+	}
 }
