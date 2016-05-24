dd4b2ea98fb1ce9d4d8ed53bb816f29066b63c17
xMedox <tim@g-lotz.de>
2016-05-24 08:04:48 +0200

    Made the second variable final

--- src/net/medox/neonengine/core/Time.java
+++ src/net/medox/neonengine/core/Time.java
@@ -3,7 +3,7 @@ package net.medox.neonengine.core;
 import org.lwjgl.glfw.GLFW;
 
 public class Time{
-	private static float SECOND = 60*0.016666668f;
+	private static final float SECOND = 60*0.016666668f;
 	
 	public static double getTime(){
 		return GLFW.glfwGetTime();
